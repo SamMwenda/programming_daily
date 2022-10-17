ee4537ba8717de7892e9654b9d3384f5fe7aa0fc
thatguyskullie <skmwenda47@gmail.com>
2022-10-17 13:48:57 +0300

    first commit

---
+++ daily0x01/bin/daily0x01.dart
@@ -0,0 +1,5 @@
+import 'package:daily0x01/daily0x01.dart' as daily0x01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${daily0x01.calculate()}!');
+}
---
+++ daily0x01/test/daily0x01_test.dart
@@ -0,0 +1,8 @@
+import 'package:daily0x01/daily0x01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
